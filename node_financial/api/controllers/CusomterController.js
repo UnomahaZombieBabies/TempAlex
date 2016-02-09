e54897f6e3c587487816a4b12b2f887e2e1bfaf7
UNO User <unouser@unomaha.edu>
2016-02-08 18:46:35 -0600

    sails generate api customer

---
+++ node_financial/api/controllers/CusomterController.js
@@ -0,0 +1,11 @@
+/**
+ * CusomterController
+ *
+ * @description :: Server-side logic for managing cusomters
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
